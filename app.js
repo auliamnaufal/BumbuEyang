4c0ab43f87200acc55c6d874f0ef2a1ec1483b04
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2021-04-20 13:45:54 +0700

    (refactor): tweak sizing of every component in home page

--- app.js
+++ app.js
@@ -1,117 +1,117 @@
 const slider = function () {
-    const slides = document.querySelectorAll('.slide');
-    const btnLeft = document.querySelector('.slider__btn--left');
-    const btnRight = document.querySelector('.slider__btn--right');
-    const dotContainer = document.querySelector('.dots');
-  
-    let curSlide = 0;
-    const maxSlide = slides.length;
-  
-    // Functions
-    const createDots = function () {
-      slides.forEach(function (_, i) {
-        dotContainer.insertAdjacentHTML(
-          'beforeend',
-          `<button class="dots__dot" data-slide="${i}"></button>`
-        );
-      });
-    };
-  
-    const activateDot = function (slide) {
-      document
-        .querySelectorAll('.dots__dot')
-        .forEach(dot => dot.classList.remove('dots__dot--active'));
-  
-      document
-        .querySelector(`.dots__dot[data-slide="${slide}"]`)
-        .classList.add('dots__dot--active');
-    };
-  
-    const goToSlide = function (slide) {
-      slides.forEach(
-        (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  const slides = document.querySelectorAll(".slide");
+  const btnLeft = document.querySelector(".slider__btn--left");
+  const btnRight = document.querySelector(".slider__btn--right");
+  const dotContainer = document.querySelector(".dots");
+
+  let curSlide = 0;
+  const maxSlide = slides.length;
+
+  // Functions
+  const createDots = function () {
+    slides.forEach(function (_, i) {
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots__dot" data-slide="${i}"></button>`
       );
-    };
-  
-    // Next slide
-    const nextSlide = function () {
-      if (curSlide === maxSlide - 1) {
-        curSlide = 0;
-      } else {
-        curSlide++;
-      }
-  
-      goToSlide(curSlide);
-      activateDot(curSlide);
-    };
-  
-    const prevSlide = function () {
-      if (curSlide === 0) {
-        curSlide = maxSlide - 1;
-      } else {
-        curSlide--;
-      }
-      goToSlide(curSlide);
-      activateDot(curSlide);
-    };
-  
-    const init = function () {
-      goToSlide(0);
-      createDots();
-  
-      activateDot(0);
-    };
-    init();
-  
-    // Event handlers
-    btnRight.addEventListener('click', nextSlide);
-    btnLeft.addEventListener('click', prevSlide);
-  
-    document.addEventListener('keydown', function (e) {
-      if (e.key === 'ArrowLeft') prevSlide();
-      e.key === 'ArrowRight' && nextSlide();
-    });
-  
-    dotContainer.addEventListener('click', function (e) {
-      if (e.target.classList.contains('dots__dot')) {
-        const { slide } = e.target.dataset;
-        goToSlide(slide);
-        activateDot(slide);
-      }
     });
   };
-  slider();
 
-  var prevScrollpos = window.pageYOffset;
-  window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-      document.getElementById("navbar").style.top = "0";
+  const activateDot = function (slide) {
+    document
+      .querySelectorAll(".dots__dot")
+      .forEach((dot) => dot.classList.remove("dots__dot--active"));
+
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add("dots__dot--active");
+  };
+
+  const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+  };
+
+  // Next slide
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
     } else {
-      document.getElementById("navbar").style.top = "-100px";
+      curSlide++;
     }
-    prevScrollpos = currentScrollPos;
+
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+    activateDot(curSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+    createDots();
+
+    activateDot(0);
+  };
+  init();
+
+  // Event handlers
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") prevSlide();
+    e.key === "ArrowRight" && nextSlide();
+  });
+
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots__dot")) {
+      const { slide } = e.target.dataset;
+      goToSlide(slide);
+      activateDot(slide);
+    }
+  });
+};
+slider();
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
   }
+  prevScrollpos = currentScrollPos;
+};
 
-const bumbuBiang = document.getElementById('biang');
-const bumbuGulai = document.getElementById('gulai');
-const bumbuRendang = document.getElementById('rendang');
-const popup = document.getElementById('popup');
-const outliner = document.getElementById('');
-const exitButton = document.getElementById('exitBtn')
+const bumbuBiang = document.getElementById("biang");
+const bumbuGulai = document.getElementById("gulai");
+const bumbuRendang = document.getElementById("rendang");
+const popup = document.getElementById("popup");
+const outliner = document.getElementById("");
+const exitButton = document.getElementById("exitBtn");
 
-bumbuBiang.addEventListener('click', () => {
-    popup.classList.remove('popup')
+bumbuBiang.addEventListener("click", () => {
+  popup.classList.remove("popup");
 });
 
-bumbuGulai.addEventListener('click', () => {
-    popup.classList.remove('popup')
+bumbuGulai.addEventListener("click", () => {
+  popup.classList.remove("popup");
 });
 
-bumbuRendang.addEventListener('click', () => {
-    popup.classList.remove('popup')         
+bumbuRendang.addEventListener("click", () => {
+  popup.classList.remove("popup");
 });
 
-exitButton.addEventListener('click', () => {
-    popup.classList.add('popup');
-})
+exitButton.addEventListener("click", () => {
+  popup.classList.add("popup");
+});
