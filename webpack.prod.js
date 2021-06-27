b0349b3e4b62132f58bf5b988d1af1eb60ea2766
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2021-06-27 21:46:21 +0700

    (build): added cleanwebpackplugin to prod webpack config file

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,5 @@
 const { merge } = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
@@ -19,4 +20,7 @@ module.exports = merge(common, {
       },
     ],
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
 });
