77f10961d8d161a0197521c36da1ea593301384b
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2021-06-28 11:19:56 +0700

    (refactor): added menu toggle button for mobile and change navbar brand size unit to vw

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,3 +3,15 @@ import 'regenerator-runtime';
 import '../styles/main.scss';
 
 console.log('Hello Coders!');
+
+document.querySelector('#menu').addEventListener('click', (e) => {
+  document.querySelector('.navbar').classList.toggle('open');
+  document.querySelector('.menu').classList.toggle('active');
+  e.stopPropagation();
+});
+
+document.querySelector('main').addEventListener('click', (e) => {
+  document.querySelector('.navbar').classList.remove('open');
+  document.querySelector('.menu').classList.remove('active');
+  e.stopPropagation();
+});
