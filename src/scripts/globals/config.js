926cc64dc6ed8cd3d41a635fbe6e0fc54c41b1a4
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2021-06-30 14:05:15 +0700

    (fix): fixed multiple identifier for buy button

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
   BASE_WA_URL: 'https://wa.me/6281770887854?text=',
+  BASE_SHOPEE_URL: 'https://bit.ly/bumbu',
 
 };
 
--- src/scripts/utils/paymentPopupInitiator.js
+++ src/scripts/utils/paymentPopupInitiator.js
@@ -16,6 +16,7 @@ const PaymentPopupInitiator = {
 
     exitBtn.addEventListener('click', (e) => {
       this._closePopup(popup, e);
+      location.reload();
     });
   },
 
@@ -38,24 +39,15 @@ const PaymentPopupInitiator = {
   },
 
   _openBuyMethod(identifier, buyMethod) {
-    const text = 'text otomatis dari website bumbu eyang, anda membeli bumbu ';
+    const text = 'Halo Bumbu Eyang, saya ingin pesan bumbu ';
     const encodedText = text.split(' ').join('%20');
 
-    switch (buyMethod) {
-      case 'whatsapp':
-        window.open(`${CONFIG.BASE_WA_URL}${encodedText}${identifier}`, '_blank');
-        break;
+    if (buyMethod === 'whatsapp') {
+      window.open(`${CONFIG.BASE_WA_URL}${encodedText}${identifier}`, '_blank');
+    }
 
-      case 'tokopedia':
-        window.open('https://www.tokopedia.com/bumbueyang', '_blank');
-        break;
-
-      case 'shopee':
-        window.open('https://shopee.co.id/shop/290263518/search?originalCategoryId=100808&page=0', '_blank');
-        break;
-
-      default:
-        break;
+    if (buyMethod === 'shopee') {
+      window.open(`${CONFIG.BASE_SHOPEE_URL}${identifier}s`, '_blank');
     }
   },
 
