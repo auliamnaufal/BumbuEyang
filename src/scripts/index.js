71ad31b3c41a4d122772a1416df11684048f6cc1
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2021-07-02 17:11:17 +0700

    (test): service worker using generate sw

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -28,5 +28,13 @@ window.addEventListener('hashchange', () => {
 
 window.addEventListener('load', () => {
   app.renderPage();
-  swRegister();
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+        console.log('SW registered: ', registration);
+      }).catch((registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      });
+    });
+  }
 });
