e12b7bce56517c7d362a91326d621ba437cf0720
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2021-07-03 11:14:04 +0700

    (style): resized and rearranged home page

--- src/scripts/components/drawer.js
+++ src/scripts/components/drawer.js
@@ -14,7 +14,7 @@ class DrawerBar extends HTMLElement {
 						<ul class="primary-nav">
 							<li><a href="#product">Produk</a></li>
 							<li><a href="#recipe">Resep</a></li>
-							<li><a href="#pembelian">Cara Pembelian</a></li>
+							<li><a href="#/buying-procedure">Cara Pembelian</a></li>
 							<li><a href="#/contact">Hubungi Kami</a></li>
 						</ul>
 					</nav>
